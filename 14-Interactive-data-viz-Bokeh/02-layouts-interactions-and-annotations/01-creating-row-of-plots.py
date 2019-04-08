23a2047ceacadc48b64b73621e0061f6d06b1fec
Johnny Fang <fangdejavu@gmail.com>
2019-04-08 10:40:41 -0500

    module 14 section 2 - Layouts Interactions & Annotations

--- 14-Interactive-data-viz-Bokeh/02-layouts-interactions-and-annotations/01-creating-row-of-plots.py
+++ 14-Interactive-data-viz-Bokeh/02-layouts-interactions-and-annotations/01-creating-row-of-plots.py
@@ -21,3 +21,24 @@ Instructions
     Put p1 and p2 into a horizontal layout using row().
     Click 'Submit Answer' to output the file and show the figure.
 '''
+# Import row from bokeh.layouts
+from bokeh.layouts import row
+
+# Create the first figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to p1
+p1.circle('fertility', 'female_literacy', source=source)
+
+# Create the second figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to p2
+p2.circle('population', 'female_literacy', source=source)
+
+# Put p1 and p2 into a horizontal row: layout
+layout = row(p1,p2)
+
+# Specify the name of the output_file and show the result
+output_file('fert_row.html')
+show(layout)
