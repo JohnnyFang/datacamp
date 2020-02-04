0fa8fa7682c23b0eb07bd03e4b75f5b77aeafa75
Johnny Fang <fangdejavu@gmail.com>
2020-02-04 15:54:19 -0500

    last ch. of unsupervised learning in Python

--- unsupervised-learning-in-python/03-decorrelating-your-data-and-dimension-reduction/08-clustering-wikipedia-part-ii.py
+++ unsupervised-learning-in-python/03-decorrelating-your-data-and-dimension-reduction/08-clustering-wikipedia-part-ii.py
@@ -14,3 +14,17 @@ Instructions
     Use the .sort_values() method of df to sort the DataFrame by the 'label' column, and print the result.
     Hit 'Submit Answer' and take a moment to investigate your amazing clustering of Wikipedia pages!
 """
+# Import pandas
+import pandas as pd
+
+# Fit the pipeline to articles
+pipeline.fit(articles)
+
+# Calculate the cluster labels: labels
+labels = pipeline.predict(articles)
+
+# Create a DataFrame aligning labels and titles: df
+df = pd.DataFrame({'label': labels, 'article': titles})
+
+# Display df sorted by cluster label
+print(df.sort_values('label'))
